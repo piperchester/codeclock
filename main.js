990669f7f39f0544304918c3575e8230ac4a7e8d
Piper Chester <piperchester@gmail.com>
2013-04-15 23:38:00 -0400

    Cleaning up some JS, making progress on button

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
-$(function() {
-	var startTop = $("#nav1").outerHeight() + $("#nav1").position().top;
-	var startLeft = $("#nav1").position().left;
-	$("#underline").attr("style","top: " + startTop + "px; left: " + startLeft + "px;");
+/**
+ * Original Author: Piper Chester
+ *
+ * Creation Date: 4.14.2013
+ */
 
-	$(".navButton").click(function() {
-		var newPos = $(this).position().left;
-		$("#underline").animate({left: newPos},"slow");
-	});
-});
-
-function init() {
-	var header = document.getElementById('myCanvas');
-	var header_height = header.offsetHeight;
-	header.style.height = '10px';
-	header.style.overflow = 'hidden';
-	var i = 0;
-	var animation = setInterval(function () {
-		if (i <= header_height) {
-			header.height = (10 + i) + 'px';
-		} else {
-			clearInterval(animation);
-			header.style.overflow = 'block';
-		}
-		i++;
-	}, 10); 
+/**
+ * Takes the input from a button click, and will convert
+ * it into a height to adjust the hour graph.
+ */
+function recordHours() {
+    var input = document.getElementById('filename'),
+        fileName = input.value;
+    var bar = document.getElementById('codeDay');
+    if (fileName) {
+        var heightToChange = input.value * 100;
+        bar.style.height = '200px';
+    } else {
+        alert('Please enter some hours!');
+        input.focus();
+    }
 }
