b4ea924037129314a783bffa860487bad331f8a6
Piper Chester <piperchester@gmail.com>
2013-05-08 00:41:58 -0400

    Minor tweaks to main.js

--- main.js
+++ main.js
@@ -14,28 +14,39 @@
     var bar = document.getElementById('codeDay');
     if (fileName) {
         var heightToChange = input.value * 100;
-        animate();
+        if (heightToChange > 1000){
+            heightToChange = 1000;
+        }
+        animate(heightToChange);
     } else {
         alert('Please enter some hours!');
         input.focus();
     }
+
+    assignDate('codeDay', fileName);
+}
+
+function animate(heightToChange){
+  $("#codeDay").animate({
+    width: "50%",
+    height: heightToChange,
+    opacity: 1,
+        // fontSize: "3em",
+        // borderWidth: "10px"
+    }, 500 );
+
+  $("#recorder").animate({
+    opacity: 0
+}, 500);
+}
+
+function assignDate(divToAssign, fileName){
     var currentDate = new Date();
     var day = currentDate.getDate();
     var month = currentDate.getMonth() + 1;
     var year = currentDate.getFullYear();
     var formatDate = "<b>" + day + "/" + month + "/" + year + "</b>";
-    document.getElementById('codeDay').innerHTML = formatDate +": " + fileName + " Hours!";
-}
-
-function animate(){
-
-  $("#codeDay").animate({
-    width: "50%",
-    height: "300px",
-    opacity: 1,
-        // fontSize: "3em",
-        // borderWidth: "10px"
-    }, 500 );
+    document.getElementById(divToAssign).innerHTML = formatDate +": " + fileName + " Hours!";
 }
 
 
